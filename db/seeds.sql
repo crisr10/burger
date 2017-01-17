dd2532426cc7c7f585464b3ca870a3d1eeea3fe0
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-01-16 16:57:11 -0800

    Something cool

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Phat', 1);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Veggie', 1 );
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Mayhem', 1);
