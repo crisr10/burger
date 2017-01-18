3e5d5fb662d98097777a2730052659f8320a84df
Cristian Rodriguez <cristian_rocas@hotmail.com>
2017-01-17 18:47:16 -0800

    App works, needs styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,36 @@
 var express = require('express');
+var burger = require('../models/burger.js');
+var router = express.Router();
+
+router.get('/index', function (req,res) {
+	burger.selectAll(function(data) {
+		var hbsObject = {
+			burgers: data
+		};
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/index', function (req, res){
+	console.log('BURGER NAME: '+req.body.burger_name);
+	burger.insertOne([
+		'burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function(){
+			res.redirect('/index');
+		});
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function() {
+    res.redirect("/index");
+  });
+});
+
+module.exports = router;
 
